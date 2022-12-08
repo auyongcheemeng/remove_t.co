79885202a1c4ed8aec2775f7da4a4e1bfc8c129e
Chee Meng Au Yong <auyongcheemeng@gmail.com>
2022-12-08 13:49:50 +0800

    Update removetco.user.js

--- removetco.user.js
+++ removetco.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name remove t.co
 // @description  Bypass t.co redirection from Twitter external links
-// @namespace https://github.com/kkren
+// @namespace https://github.com/auyongcheemeng
 // @match *://twitter.com/*
+// @match *://tweetdeck.twitter.com/*
 // @grant none
 // @require https://cdn.jsdelivr.net/npm/jquery@2.2.4/dist/jquery.min.js
 // @run-at document-end
-// @downloadURL https://raw.githubusercontent.com/kkren/remove_t.co/master/removetco.user.js
-// @version 0.5
+// @downloadURL https://raw.githubusercontent.com/auyongcheemeng/remove_t.co/master/removetco.user.js
+// @version 0.6
 // ==/UserScript==
 
 
@@ -21,9 +22,16 @@ function replace() {
       var expanded = $("[href*='t.co']").eq(i).attr("title");
     }
     $("[href*='t.co']").eq(i).attr("href", expanded);
+
+    if ($("[href*='t.co']").eq(i).attr("data-full-url") != undefined) {
+      var expanded = $("[href*='t.co']").eq(i).attr("data-full-url");
+    } else {
+      var expanded = $("[href*='t.co']").eq(i).attr("title");
+    }
+    $("[href*='t.co']").eq(i).attr("href", expanded);
   }
 }
-//滚动加载
+//
 mo = new MutationObserver(function(allmutations) {
   //alert();
   replace();
